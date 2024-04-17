adc2019374a150a6785eed5d331c313363e895c4
Davias Leaptoken <115111841+LeapToken@users.noreply.github.com>
2024-04-16 22:40:59 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 $('.show-to').click(function(){
   $('.show-to2').slideToggle();
 });
+
+function togglePasswordVisibility() {
+    var passwordInput = document.getElementById('password');
+    var type = passwordInput.getAttribute('type') === 'password' ? 'text' : 'password';
+    passwordInput.setAttribute('type', type);
+}
