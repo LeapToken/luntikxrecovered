4079a28a329a8e0c47bf905c677e0912cd53c4f2
Davias Leaptoken <115111841+LeapToken@users.noreply.github.com>
2024-04-18 20:17:18 -0600

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -127,6 +127,16 @@ if(!isset($_SESSION['user'])){
       padding: 20px;
       text-align: center;
     }
+    .dragme{
+			position:relative;
+			width: 270px;
+			height: 203px;
+			cursor: move;
+		}
+	  #draggable {
+			background-color: #ccc;
+			border: 1px solid #000;
+		}
   </style>
 </head>
 <body>
@@ -170,6 +180,8 @@ if(!isset($_SESSION['user'])){
       <span>Плейлист с Геймплеем</span>
     </button>
   </div>
+  
+  <img src="https://cdn.glitch.global/1c9ce23e-3e3d-45e5-baa7-98b679b83fc0/image1773-grir8nkt.png?v=1713492905665" alt="drag-and-drop image script" class="dragme">
 
   <div class="help-text">Если вы нашли ошыбку на сайте, сообщите мне! :)</div>
   
@@ -184,6 +196,50 @@ if(!isset($_SESSION['user'])){
     }
     isTabOpen = !isTabOpen;
   }
+  function startDrag(e) {
+				// determine event object
+				if (!e) {
+					var e = window.event;
+				}
+
+				// IE uses srcElement, others use target
+				var targ = e.target ? e.target : e.srcElement;
+
+				if (targ.className != 'dragme') {return};
+				// calculate event X, Y coordinates
+					offsetX = e.clientX;
+					offsetY = e.clientY;
+
+				// assign default values for top and left properties
+				if(!targ.style.left) { targ.style.left='0px'};
+				if (!targ.style.top) { targ.style.top='0px'};
+
+				// calculate integer values for top and left 
+				// properties
+				coordX = parseInt(targ.style.left);
+				coordY = parseInt(targ.style.top);
+				drag = true;
+
+				// move div element
+					document.onmousemove=dragDiv;
+				
+        }
+        function dragDiv(e) {
+          if (!drag) {return};
+          if (!e) { var e= window.event};
+          var targ=e.target?e.target:e.srcElement;
+          // move div element
+          targ.style.left=coordX+e.clientX-offsetX+'px';
+          targ.style.top=coordY+e.clientY-offsetY+'px';
+          return false;
+        }
+        function stopDrag() {
+          drag=false;
+        }
+        window.onload = function() {
+          document.onmousedown = startDrag;
+          document.onmouseup = stopDrag;
+        }
 </script>
 
 </body>
